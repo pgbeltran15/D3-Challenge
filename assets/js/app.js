bb92f73a47ed1b100574ff8b03ff90857afe716c
Patrick Beltran <66067267+pgbeltran15@users.noreply.github.com>
2020-10-05 08:43:28 -0400

    set up margins

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,77 +1,17 @@
 // @TODO: YOUR CODE HERE!
-var svgWidth = 960;
-var svgHeight = 500;
+// Set up SVG definitions
+let svgWidth = 960;
+let svgHeight = 620;
 
-var margin = {
-  top: 20,
-  right: 40,
-  bottom: 80,
+// set up borders in svg
+let margin = {
+  top: 20, 
+  right: 40, 
+  bottom: 200,
   left: 100
 };
 
-var width = svgWidth - margin.left - margin.right;
-var height = svgHeight - margin.top - margin.bottom;
-
-// Create an SVG wrapper, append an SVG group that will hold our chart,
-// and shift the latter by left and top margins.
-var svg = d3
-  .select(".chart")
-  .append("svg")
-  .attr("width", svgWidth)
-  .attr("height", svgHeight);
-
-// Append an SVG group
-var chartGroup = svg.append("g")
-  .attr("transform", `translate(${margin.left}, ${margin.top})`);
-
-// Initial Params
-var chosenXAxis = "poverty";
-var chosenYAxis = "obesity";
-
-
-// function used for updating x-scale var upon click on axis label
-function xScale(stateData, chosenXAxis) {
-    // create scales
-    var xLinearScale = d3.scaleLinear()
-      .domain([d3.min(stateData, d => d[chosenXAxis]) * 0.8,
-        d3.max(stateData, d => d[chosenXAxis]) * 1.2
-      ])
-      .range([0, width]);
-  
-    return xLinearScale;
-  
-  }
-
-function yScale(stateData, chosenYAxis) {
-    // create scales
-    var yLinearScale = d3.scaleLinear()
-      .domain([d3.min(stateData, d => d[chosenYAxis]) * 0.8,
-        d3.max(stateData, d => d[chosenYAxis]) * 1.2
-      ])
-      .range([0, width]);
-  
-    return yLinearScale;
-  
-  }
-
-// function used for updating xAxis and yAxis var upon click on axis label
-function renderXAxis(newXScale, xAxis) {
-    var bottomAxis = d3.axisBottom(newXScale);
-  
-    xAxis.transition()
-      .duration(1000)
-      .call(bottomAxis);
-  
-    return xAxis;
-  }
-
-function renderYAxis(newXScale, yAxis) {
-    var leftAxis = d3.axisLeft(newYScale);
-  
-    yAxis.transition()
-      .duration(1000)
-      .call(leftAxis);
-  
-    return yAxis;
-  }
+// calculate chart height and width
+let width = svgWidth - margin.right - margin.left;
+let height = svgHeight - margin.top - margin.bottom;
 
